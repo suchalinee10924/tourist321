f636dc0d81077d2e6c03ef37fae68fb3a14bc745
suchjam <158018835+suchalinee10924@users.noreply.github.com>
2024-05-13 16:41:57 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+    var tourpoint = {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Khao Yai National Park",
+              "type": "natural",
+              "level": 5
+            },
+            "geometry": {
+              "coordinates": [
+                101.50390028482593,
+                14.334004561986362
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Grand Palace",
+              "type": "cutural",
+              "level": 4
+            },
+            "geometry": {
+              "coordinates": [
+                100.49159680271276,
+                13.751055988352235
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "ICON Siam",
+              "type": "spacial",
+              "level": 3
+            },
+            "geometry": {
+              "coordinates": [
+                100.51043650000179,
+                13.726928286123496
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Lumphini Park",
+              "type": "natural",
+              "level": 4
+            },
+            "geometry": {
+              "coordinates": [
+                100.54162285676449,
+                13.731290013846163
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Benjakitti Forest Park",
+              "type": "natural",
+              "level": 4
+            },
+            "geometry": {
+              "coordinates": [
+                100.55476428492523,
+                13.72964539334616
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Central Rama 9",
+              "type": "spacial",
+              "level": 3
+            },
+            "geometry": {
+              "coordinates": [
+                100.56604222883288,
+                13.7585922568273
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Wang lang market",
+              "type": "market ",
+              "level": 5
+            },
+            "geometry": {
+              "coordinates": [
+                100.48529751628104,
+                13.75547739668933
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Chatuchak Market",
+              "type": "market",
+              "level": 5
+            },
+            "geometry": {
+              "coordinates": [
+                100.55048842726086,
+                13.799866203058144
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Wat Phra Kaew",
+              "type": "natural",
+              "level": 5
+            },
+            "geometry": {
+              "coordinates": [
+                100.49270676147353,
+                13.751608495858974
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Wat Arun",
+              "type": "natural",
+              "level": 5
+            },
+            "geometry": {
+              "coordinates": [
+                100.48889699797832,
+                13.743734938129634
+              ],
+              "type": "Point"
+            }
+          }
+        ]
+      }
